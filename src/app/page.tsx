4298a8b67f8456ef4a99a0a2cda7b6e9ffc22828
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-26 23:39:57 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/organisms/Navbar";
-import Title from "@/components/atoms/Title";
 import Footer from "@/components/organisms/Footer";
-import Herosection from "@/components/organisms/Herosection";
 export default function page() {
   return (
     <div>
       <Navbar />
-      <Herosection />
+  
       <Footer />
     </div>
   );
