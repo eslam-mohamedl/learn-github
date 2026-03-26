5a229fdc21e15626dde28732c52ddf9eb370cb8d
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-26 23:10:17 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Title from "@/components/atoms/Title";
+
+export default function page() {
+  return (
+    <div>
+      <Title variant="primary" size="md">
+       title sdfsdf
+      </Title>
+    </div>
+  );
+}
