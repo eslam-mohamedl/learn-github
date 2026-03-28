03a593395e7465ca82e36655ad97df984fa7370f
Zakaria Moustakim <zakariamous97@gmail.com>
2026-03-28 21:27:55 +0000

    resolve conflict - keep only team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from "react";
-import TeamSection from "../components/organisms/Teamsection";
-=======
-import Herosection from "@/components/organisms/Herosection";
-import CardSection from "@/components/organisms/CardSection";
-import FAQSection from "@/components/organisms/FAQSection";
-import AboutSection from "@/components/organisms/AboutSection";
->>>>>>> 37d57c0583b62a7a69d9af5e278178a84bf7dcf7
-=======
-import Herosection from "@/components/organisms/Herosection";
-import CardSection from "@/components/organisms/CardSection";
-import Pricingsection from "@/components/organisms/Pricingsection";
-import Footer from "@/components/organisms/Footer";
-import FeaturesSection from "@/components/organisms/FeaturesSection";
-import FAQSection from "@/components/organisms/FAQSection";
-import AboutSection from "@/components/organisms/AboutSection";
-import TestimonialsSection from "@/components/organisms/TestimonialsSection";
-import TestimonialsSection from "@/components/organisms/TeamSection";
+import TeamSection from "@/components/organisms/TeamSection";
 
->>>>>>> 34c6794bcc97ed6c404b1eb2673d6930fd0b2120
-
-export default function page() {
+export default function Page() {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <TeamSection />
-=======
-      <Herosection />
-      <CardSection />
-
-      <FAQSection/>
-
-      <AboutSection />
-
->>>>>>> 37d57c0583b62a7a69d9af5e278178a84bf7dcf7
     </div>
   );
 }
-=======
-      <Herosection />
-      <CardSection />
-      <Pricingsection />
-      <Footer />
-      <FeaturesSection />
-      <FAQSection/>
-      <AboutSection />
-      <TestimonialsSection />
-            <TeamSection />
-
-
-
-    </div>
-  );
-}
->>>>>>> 34c6794bcc97ed6c404b1eb2673d6930fd0b2120
