898caf2692645b6394aa9838239b7a85bd209c12
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-03-28 23:01:23 +0200

    Features Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
+import FeaturesSection from "@/components/organisms/FeaturesSection";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
+      <FeaturesSection />
     </div>
   );
 }
