19d2b2eacddb91df67f541bfe015f50178186a3e
Zakaria Moustakim <zakariamous97@gmail.com>
2026-03-28 22:19:19 +0000

    new zakaria branch commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import TeamSection from "./teamsection";
+import Herosection from "@/components/organisms/Herosection";
+import CardSection from "@/components/organisms/CardSection";
+import Pricingsection from "@/components/organisms/Pricingsection";
+import Footer from "@/components/organisms/Footer";
+import FeaturesSection from "@/components/organisms/FeaturesSection";
+import FAQSection from "@/components/organisms/FAQSection";
+import AboutSection from "@/components/organisms/AboutSection";
+import TestimonialsSection from "@/components/organisms/TestimonialsSection";
+import ContactSection from "@/components/organisms/ContactSection";
+import Testomonials from "@/components/organisms/Testemonials";
 
 export default function page() {
   return (
     <div>
       <TeamSection />
+      <Herosection />
+      <CardSection />
+      <Pricingsection />
+      <Footer />
+      <FeaturesSection />
+      <FAQSection />
+      <AboutSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Testomonials />
     </div>
   );
 }
---
+++ src/components/organisms/Pricingsection.tsx
@@ -0,0 +1,7 @@
+export default function Pricingsection() {
+  return (
+    <div className="bg-blue-400 text-white p-5  flex  justify-center items-center ">
+      Pricingsection
+    </div>
+  );
+}
