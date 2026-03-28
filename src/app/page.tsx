0a1f071ff1a9eef323e691c6da99b20224b205ce
lamiaAtef <90112559+lamiaAtef@users.noreply.github.com>
2026-03-28 22:59:35 +0200

    add faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
+import FAQSection from "@/components/organisms/FAQSection";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
+      <FAQSection/>
     </div>
   );
 }
