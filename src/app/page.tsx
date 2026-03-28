c0700dd6a6a64cf06d01eced94ba7f7b49f14792
Nader-Dev1 <nder0028@gmail.com>
2026-03-28 14:35:25 -0700

    add servise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
+import ServiseSection from "@/components/organisms/Servisesection";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
+      <ServiseSection/>
     </div>
   );
 }
