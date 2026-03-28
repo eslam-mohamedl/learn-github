d32e62bee40f17dbc87ddad5befc0020b11c9d1b
Mustafa Khaled <mustafakh565@gmail.com>
2026-03-28 22:57:43 +0200

    add aboutSection component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
+import AboutSection from "@/components/organisms/AboutSection";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
+      <AboutSection />
     </div>
   );
 }
