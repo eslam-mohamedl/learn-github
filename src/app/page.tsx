49c3bfaadfcb6bf28aba47b30107c3a82e21baf7
Hanan <Hanan.bayazeed56@gmail.com>
2026-03-29 00:09:03 +0300

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
 import Pricingsection from "@/components/organisms/Pricingsection";
+import Footer from "@/components/organisms/Footer";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
       <Pricingsection />
+      <Footer />
     </div>
   );
 }
