bd0deb560d6518f65ea4cbe815c524ea30afa353
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-03-28 22:57:14 +0200

    built testimonials section

---
+++ src/components/organisms/TestimonialsSection.tsx
@@ -0,0 +1,7 @@
+export default function TestimonialsSection() {
+  return (
+    <div className="bg-gray-400 text-white p-5  flex  justify-center items-center ">
+        TestimonialsSection
+    </div>
+  )
+}
