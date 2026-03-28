498185ab79e112b7a6e7e73476f8fb04a8f5e65e
Hanan <Hanan.bayazeed56@gmail.com>
2026-03-29 00:06:54 +0300

    pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
+import Pricingsection from "@/components/organisms/Pricingsection";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
+      <Pricingsection />
     </div>
   );
 }
