6c14e620947d511f2e476cb90de007a8bbbcf32c
SawsanAllam <sawsanallam.332@gmail.com>
2026-03-28 23:20:38 +0200

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Herosection from "@/components/organisms/Herosection";
 import CardSection from "@/components/organisms/CardSection";
 import AboutSection from "@/components/organisms/AboutSection";
+import Navbar from "@/components/organisms/Navbar";
 export default function page() {
   return (
     <div>
       <Herosection />
       <CardSection />
       <AboutSection />
+      <Navbar/>
     </div>
   );
 }
