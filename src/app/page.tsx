0cf5a6aa02c4b2458f49faa0e0342971b108af38
Zakaria Moustakim <zakariamous97@gmail.com>
2026-03-28 21:17:19 +0000

    add team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+<<<<<<< HEAD
 import React from "react";
 import TeamSection from "./teamsection";
+=======
+import Herosection from "@/components/organisms/Herosection";
+import CardSection from "@/components/organisms/CardSection";
+import FAQSection from "@/components/organisms/FAQSection";
+import AboutSection from "@/components/organisms/AboutSection";
+>>>>>>> 37d57c0583b62a7a69d9af5e278178a84bf7dcf7
 
 export default function page() {
   return (
     <div>
+<<<<<<< HEAD
       <TeamSection />
+=======
+      <Herosection />
+      <CardSection />
+
+      <FAQSection/>
+
+      <AboutSection />
+
+>>>>>>> 37d57c0583b62a7a69d9af5e278178a84bf7dcf7
     </div>
   );
 }
